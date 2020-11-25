668de230c6795748bf736534b457c270676b0b96
GustavoExel <gustavoexelgpe@gmail.com>
2020-11-25 15:04:42 -0300

    implemented OOP stress_equilibrium.py

--- apps/stress_equilibrium.py
+++ apps/stress_equilibrium.py
@@ -116,7 +116,6 @@ class StressEquilibriumSolver(Solver):
 									self.add( W(neighborVertex), V(vertex.handle), matrixCoefficient[2][1][local] )
 								matrixCoefficient *= -1
 
-
 		def boundaryConditions():
 			# Boundary Conditions
 			for bc in self.problemData.boundaryConditions:
@@ -125,79 +124,48 @@ class StressEquilibriumSolver(Solver):
 				vBoundaryType = bc["v"].__type__
 				wBoundaryType = bc["w"].__type__ if "w" in bc.keys() else None
 
-				SÓ PRA RECAPITULAR, PARECEMOS QUE ESTAMOS COM UM CÓGIDO EM STRESS EQUILIBRIUM2
-				IGUAL AO STRESS EQUILIBRIUM QUE FAZEM COISAS DIFERENTES
+				# Neumann Boundary Conditions
+				if uBoundaryType == "NEUMANN":
+					for facet in boundary.facets:
+						for outerFace in facet.outerFaces:
+							self.independent[U(outerFace.vertex.handle)] -= bc["u"].getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
 
-				A DIFERENÇA ESTÁ SÓ NAS CONDIÇÕES DE FRONTEIRA
-				ESSE ESTÁ ADICIONANDO ALGUNS TERMOS QUE O SE2 NÃO ADICIONA, MAS O SE2 TB
-				ADICIONA UNS TERMOS QUE NÃO TEM AQUI
+				if vBoundaryType == "NEUMANN":
+					for facet in boundary.facets:
+						for outerFace in facet.outerFaces:
+							self.independent[V(outerFace.vertex.handle)] -= bc["v"].getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
 
-				EM SE2 TEM UMA FUNÇÃOZINHA QUE SALVA A MATIZ ESPARSA EM UMA PLANILHA DO EXCEL
-				E AQUI TEM 5 LINHAS QUE LEEM ISSO E COMPARAM
+				if wBoundaryType == "NEUMANN":
+					for facet in boundary.facets:
+						for outerFace in facet.outerFaces:
+							self.independent[W(outerFace.vertex.handle)] -= bc["w"].getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
 
-				PRECISAMOS ACHAR A DIFERENÇA ENTRE OS CÓDIGOS
+				# Dirichlet Boundary Conditions
+				if uBoundaryType == "DIRICHLET":
+					for vertex in boundary.vertices:
+						self.independent[U(vertex.handle)] = bc["u"].getValue(vertex.handle)
+						self.matrixVals = [val for coord, val in zip(self.coords, self.matrixVals) if coord[0] != U(vertex.handle)]
+						self.coords 	   = [coord for coord in self.coords if coord[0] != U(vertex.handle)]
+						self.add(U(vertex.handle), U(vertex.handle), 1.0)
 
-				-------------
+				if vBoundaryType == "DIRICHLET":
+					for vertex in boundary.vertices:
+						self.independent[V(vertex.handle)] = bc["v"].getValue(vertex.handle)
+						self.matrixVals = [val for coord, val in zip(self.coords, self.matrixVals) if coord[0] != V(vertex.handle)]
+						self.coords 	   = [coord for coord in self.coords if coord[0] != V(vertex.handle)]
+						self.add(V(vertex.handle), V(vertex.handle), 1.0)
 
-				DEPOIS QUE ESSE CÓDIGO ESTIVER FUNCIONADO TEMOS QUE VALIDAR O ESQUEMA DE
-				SOMATÓRIO DE DIVERGENTE E GRADIENTE, E ÁREA, E VOLUME, E INTEGRAL, E CAMPO ANALÍTICO,
-				TODA ESSA PARADA TEM QUE SER VALIDADA
-
-				DEPOIS AINDA QUEREMOS IMPLEMENTAR O MODELO GEOMECÂNICO COM EQUAÇÃO DA CONSERVAÇÃO DA MASSA
-
-				def neumannBoundaryConditions():
-					if uBoundaryType == "NEUMANN":
-						for facet in boundary.facets:
-							for outerFace in facet.outerFaces:
-								self.independent[U(outerFace.vertex.handle)] -= bc["u"].getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
-
-					if vBoundaryType == "NEUMANN":
-						for facet in boundary.facets:
-							for outerFace in facet.outerFaces:
-								self.independent[V(outerFace.vertex.handle)] -= bc["v"].getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
-
-					if wBoundaryType == "NEUMANN":
-						for facet in boundary.facets:
-							for outerFace in facet.outerFaces:
-								self.independent[W(outerFace.vertex.handle)] -= bc["w"].getValue(outerFace.handle) * np.linalg.norm(outerFace.area.getCoordinates())
-
-				def dirichletBoundaryConditions():
-					if uBoundaryType == "DIRICHLET":
-						for vertex in boundary.vertices:
-							self.independent[U(vertex.handle)] = bc["u"].getValue(vertex.handle)
-							self.matrixVals = [val for coord, val in zip(self.coords, self.matrixVals) if coord[0] != U(vertex.handle)]
-							self.coords 	   = [coord for coord in self.coords if coord[0] != U(vertex.handle)]
-							self.add(U(vertex.handle), U(vertex.handle), 1.0)
-
-					if vBoundaryType == "DIRICHLET":
-						for vertex in boundary.vertices:
-							self.independent[V(vertex.handle)] = bc["v"].getValue(vertex.handle)
-							self.matrixVals = [val for coord, val in zip(self.coords, self.matrixVals) if coord[0] != V(vertex.handle)]
-							self.coords 	   = [coord for coord in self.coords if coord[0] != V(vertex.handle)]
-							self.add(V(vertex.handle), V(vertex.handle), 1.0)
-
-					if wBoundaryType == "DIRICHLET":
-						for vertex in boundary.vertices:
-							self.independent[W(vertex.handle)] = bc["w"].getValue(vertex.handle)
-							self.matrixVals = [val for coord, val in zip(self.coords, self.matrixVals) if coord[0] != W(vertex.handle)]
-							self.coords 	   = [coord for coord in self.coords if coord[0] != W(vertex.handle)]
-							self.add(W(vertex.handle), W(vertex.handle), 1.0)
-
-			neumannBoundaryConditions()
-			dirichletBoundaryConditions()
+				if wBoundaryType == "DIRICHLET":
+					for vertex in boundary.vertices:
+						self.independent[W(vertex.handle)] = bc["w"].getValue(vertex.handle)
+						self.matrixVals = [val for coord, val in zip(self.coords, self.matrixVals) if coord[0] != W(vertex.handle)]
+						self.coords 	   = [coord for coord in self.coords if coord[0] != W(vertex.handle)]
+						self.add(W(vertex.handle), W(vertex.handle), 1.0)
 
 		if self.gravity:
 			gravityTerm()
 		stressTerm()
 		boundaryConditions()
-
-		import pandas as pd
-		data = pd.read_csv("matrix.csv")
-		self.dcoords = list(zip( list(np.array(data["I"])), list(np.array(data["J"])) ))
-		self.dmvals = list(np.array(data["V"]))
-		self.ccoords = self.coords.copy()
-		self.cmvals = self.matrixVals.copy()
-
 
 	def solveLinearSystem(self):
 		self.matrix = sparse.csc_matrix( (self.matrixVals, zip(*self.coords)) )
